1dd932cb74e1aecebdd63e4301b58e800fcbc4f0
Horscht <befr1013@hs-karlsruhe.de>
2012-07-22 22:12:46 +0200

    automat (3)
    Signed-off-by: Horscht <befr1013@hs-karlsruhe.de>

--- workspace/Wissenheit/src/Controller/States/State.java
+++ workspace/Wissenheit/src/Controller/States/State.java
@@ -1,31 +1,5 @@
 package Controller.States;
+
 public interface State {
 
-
-	static class Datenerfassung {
-	}
-
-
-	static class Initialisierung {
-	}
-
-
-
-	static class Spielzug {
-	}
-
-
-
-	static class Fragerunde {
-	}
-
-
-
-	static class SpielzugVorbereiten {
-	}
-
-
-
-	static class HighScoreModus {
-	}
 }

c44e699c51984969866c3b60d30c1fe2bf084128
Oliver Eichner <o.eichner@gmail.com>
2012-07-23 11:33:47 +0200

    choose und move

--- workspace/Wissenheit/src/Controller/Game.java
+++ workspace/Wissenheit/src/Controller/Game.java
@@ -1,3 +1,17 @@
 package Controller;
+
 public class Game {
+	
+	public void chooseWissensstreiter(){
+		
+	}
+	
+	
+	public void moveWissensstreiter() {
+		return;
+	}
+
+	int wuerfeln() {
+		return 0;
+	}
 }
